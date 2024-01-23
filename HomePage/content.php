ade8c01e113ad53bd1c06bf45634192a8b9c07bf
LOKALSF56 <asuryafirdaus@gmail.com>
2024-01-23 20:12:43 +0700

    commit pertama

---
+++ HomePage/content.php
@@ -0,0 +1,47 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <header>
+    <h1 class="logo"> PT. Mencari Cinta Sejati</h1>
+        <nav class="nav-container">
+            <ul class="kiri">
+                <li><a class="home" href="#">Home</a></li>
+                <li><a class="hitam" href="#">Lowongan</a></li>
+                <li><a class="hitam" href="#">Pengumuman</a></li>
+            </ul>
+            <ul class="kanan">
+                <li>
+                    <a href="#">Akun</a>
+                    <ul class="dropdown">
+                        <li><a class="Login" href="#">Login</a></li>
+                        <li><a class="Regis" href="#">Mendaftar</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main class="isiweb">
+        <div class="content">
+            <div class="isi1">
+                <img src="">
+                <div class ="list">
+                    <h1>Mari Bergabung Dengan Kami!</h1>
+                    <p>Berkarir di perusahaan kami, adalah suatu kesempatan yg luar biasa yang akan anda alami selama anda berkarir.</p>
+                </div>
+            </div>
+            <div class="isi2">
+                <img src="">
+                <div class ="list">
+                    <h1>Tempat Dimana Anda Mencari Pasangan Sejati!</h1>
+                    <p>Dengan Bekerja di perusahaan kami, tidak hanya anda mendapat karir yang sesuai dengan passion anda, tetapi mencari passangan yang ideal untuk anda</p>
+                </div>
+        </div>
+    </main>
+        <footer>
+            <p>Copyright @ 2024 PT Mencari Cinta Sejati, Designed by Kelompok 6 Pweb</p>
+        </footer>
+    </body>
+</html>
