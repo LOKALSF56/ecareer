a4133a5b73f366406d1b6586a44de1e9da65a83a
macbook <imantasiman07@gmail.com>
2024-02-15 21:27:26 +0700

    commit 3 daftar akun

--- Daftar Akun/content.php
+++ Daftar Akun/content.php
@@ -1,80 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Form Pendaftaran</title>
+    <title>Form Data Diri</title>
+
 </head>
+
 <body>
+
     <div class="container">
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <h2>Form Pendaftaran</h2>
+        <h2>Form Data Diri</h2>
+        <form action="process.php" method="post" enctype="multipart/form-data">
             <label for="nama">Nama:</label>
-            <input type="text" name="nama" required>
+            <input type="text" id="nama" name="nama" required>
+
+            <label for="ttl">Tempat Lahir:</label>
+            <input type="text" id="ttl" name="ttl" required>
+
+            <label for="tanggal_lahir">Tanggal Lahir:</label>
+            <input type="date" name="tanggal_lahir" required>
+
+            <label for="no_id">NO ID:</label>
+            <input type="text" id="no_id" name="no_id" required>
+
+            <label for="pengalaman">Pengalaman:</label>
+            <input type="text" id="pengalaman" name="pengalaman" required>
+
+            <label for="pendidikan">Pendidikan:</label>
+            <input type="text" id="pendidikan" name="pendidikan" required>
 
             <label for="email">Email:</label>
-            <input type="email" name="email" required>
-
-            <label for="no_hp">No HP:</label>
-            <input type="tel" name="no_hp" required>
+            <input type="email" id="email" name="email" required>
 
             <label for="password">Password:</label>
-            <input type="password" name="password" required>
+            <input type="password" id="password" name="password" required>
 
-            <label for="repassword">Re-Password:</label>
-            <input type="password" name="repassword" required>
+            <label for="no_hp">No HP:</label>
+            <input type="text" id="no_hp" name="no_hp" required>
 
-            <button type="submit" name="submit">Daftar</button>
+            <label for="lampiran">Lampiran CV:</label>
+            <input type="file" id="lampiran" name="lampiran">
+
+            <input type="submit" value="Submit">
         </form>
     </div>
+
     <footer>
-        <p>Copyright @ 2024 PT Mencari Cinta Sejati, Designed by Kelompok 6 Pweb</p>
+        &copy; 2024 PT Mencari Cinta Sejati, Design by Kelompok 6 Pweb
     </footer>
+
 </body>
+
 </html>
-
-<?php
-// Konfigurasi database
-$servername = "localhost";
-$username = "root"; 
-$password = ""; 
-$dbname = "ecareer";
-
-// Membuat koneksi ke database
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Memeriksa koneksi
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
-}
-
-// Memproses form ketika tombol submit ditekan
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nama = $_POST["nama"];
-    $email = $_POST["email"];
-    $no_hp = $_POST["no_hp"];
-    $password = $_POST["password"];
-    $repassword = $_POST["repassword"];
-
-    // Memeriksa apakah password cocok
-    if ($password != $repassword) {
-        echo "Password tidak cocok.";
-    } else {
-        // Enkripsi password sebelum disimpan ke database
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-        // Memasukkan data ke database
-        $sql = "INSERT INTO users (nama, email, no_hp, password) VALUES ('$nama', '$email', '$no_hp', '$hashed_password')";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "Pendaftaran berhasil.";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-    }
-    
-    // Menutup koneksi database
-    $conn->close();
-}
-?>
