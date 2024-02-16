09c462d5ccd13b677a0993494ea4a20dfaf08151
macbook <imantasiman07@gmail.com>
2024-02-16 19:21:22 +0700

    commit ke 4

--- Data Diri/process.php
+++ Data Diri/process.php
@@ -4,30 +4,39 @@ $username = "root";
 $password = ""; 
 $dbname = "ecareer";
 
+// Membuat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Memeriksa koneksi
 if ($conn->connect_error) {
     die("Koneksi ke database gagal: " . $conn->connect_error);
 }
 
-$nama = $_POST['nama'];
-$ttl = $_POST['ttl'];
-$tanggal_lahir = $_POST['tanggal_lahir'];
-$no_id = $_POST['no_id'];
-$pengalaman = $_POST['pengalaman'];
-$pendidikan = $_POST['pendidikan'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$no_hp = $_POST['no_hp'];
+// Mengambil nilai dari formulir dan melakukan sanitasi
+$nama = mysqli_real_escape_string($conn, $_POST['nama']);
+$ttl = mysqli_real_escape_string($conn, $_POST['ttl']);
+$tanggal_lahir = mysqli_real_escape_string($conn, $_POST['tanggal_lahir']);
+$no_id = mysqli_real_escape_string($conn, $_POST['no_id']);
+$pengalaman = mysqli_real_escape_string($conn, $_POST['pengalaman']);
+$pendidikan = mysqli_real_escape_string($conn, $_POST['pendidikan']);
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+$password = mysqli_real_escape_string($conn, $_POST['password']);
+$no_hp = mysqli_real_escape_string($conn, $_POST['no_hp']);
 
-$sql = "INSERT INTO nama_tabel (nama, tempat_lahir, tanggal_lahir, no_id, pengalaman, pendidikan, email, password, no_hp)
-VALUES ('$nama', '$ttl', '$tanggal_lahir', '$no_id', '$pengalaman', '$pendidikan', '$email', '$password', '$no_hp')";
+// Hash password
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
+// Menyusun query SQL INSERT
+$sql = "INSERT INTO datadiri (EMAIL, PASSWORD, NAMA_LENGKAP, NOKTP, NOHP)
+VALUES ('$email', '$hashed_password', '$nama', '$no_id', '$no_hp')";
+
+// Menjalankan query dan memeriksa hasilnya
 if ($conn->query($sql) === TRUE) {
     echo "Data berhasil disimpan";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
+// Menutup koneksi
 $conn->close();
 ?>
